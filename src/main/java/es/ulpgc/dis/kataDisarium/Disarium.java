7f75c48b18226dfd01c33c0ef2c5537f78398c76
RaulVF007 <raulvf007@gmail.com>
2021-10-28 09:41:36 +0100

    green return_false_with_number_200

--- src/main/java/es/ulpgc/dis/kataDisarium/Disarium.java
+++ src/main/java/es/ulpgc/dis/kataDisarium/Disarium.java
@@ -6,7 +6,15 @@ import java.util.List;
 public class Disarium {
 
     public boolean isDisarium(int number) {
-        return true;
+        int sum = 0;
+        int posStringNumber = 0;
+        String stringNumber = String.valueOf(number);
+
+        while(posStringNumber < stringNumber.length()){
+            sum += Math.pow(Integer.valueOf(stringNumber.substring(posStringNumber,posStringNumber + 1)),posStringNumber + 1);
+            posStringNumber++;
+        }
+        return sum == number;
     }
 
     public List<Integer> calculateDisarium(int lowerLimit, int upperLimit) {
